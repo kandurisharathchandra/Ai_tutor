557035ca7407bbe6d2f47d8c4f2867e3147b02f4
ksc3045 <sharathchandrakanduri@gmail.com>
2025-07-04 17:39:44 +0530

    just initial setup and requirements

---
+++ requirements.py
@@ -0,0 +1,25 @@
+# Core dependencies
+langchain>=0.1.0
+PyPDF2>=3.0.1
+chromadb>=0.4.24
+sentence-transformers>=2.2.2
+
+# Optional: For HuggingFace embedder
+torch>=1.13.1
+transformers>=4.36.0
+
+# Utility
+tqdm
+numpy
+streamlit
+langchain-community
+langchain_huggingface
+langchain_google_genai
+chromadb 
+sentence-transformers
+
+langchain
+pytesseract 
+pymupdf 
+pillow
+protobuf==3.20.3
